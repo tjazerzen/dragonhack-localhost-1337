b4ebc9e24c690f75676a88ea4f0cdc95b24aa552
Tim Kalan <timkalan99@gmail.com>
2025-04-13 02:00:44 +0200

    razpalitev: navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Footer from '@/components/Footer';
-import NavBar from '@/components/NavBar';
 
 const geistSans = Geist({
   subsets: ['latin'],
@@ -33,7 +32,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased font-medium`}
       >
         <div className="flex flex-col h-screen w-full">
-          <NavBar />
           <main className="flex-1 overflow-auto">
             {children}
           </main>
